82910f86b67246100de31529d57a311c6a1f6926
Kenneth Barrett <kenneth.barrett.jr@gmail.com>
2019-01-30 09:21:55 -0500

    Add access controller, admin menu, and login form

---
+++ app/controllers/access_controller.rb
@@ -0,0 +1,19 @@
+class AccessController < ApplicationController
+
+  layout 'admin'
+
+  def menu
+    # Display text and links
+  end
+
+  def login
+    # Login form
+  end
+
+  def attempt_login
+  end
+
+  def logout
+  end
+
+end
